814df91ca0975ef0819812adb31a0351d5570082
mandihamza <mandihamza@gmail.com>
2020-01-27 16:57:59 -0700

    Creates Store and adds: Provider, logger, middleware, and imports reducer

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import reducer from './reducers/index.js';
+
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, applyMiddleware(logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,document.getElementById("root"));
